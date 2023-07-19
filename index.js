64bf5fa8ae2ecaebf92250cb51bd92fde52593ac
anhtai13 <tainguyen2322001@gmail.com>
2023-07-19 21:21:36 +0700

    baitap | bài tập

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, {
-    "Content-Type": "text/html, charset=utf-8",
-  });
-
- 
-  response.write("<h1>Hello world</h1>");
-
-  response.end();
+const content = "Tín\nThái\nTrung\nSáng\nViệt Plus\nSơn\nTài\nGiang";
+fs.writeFile("test.txt", content, "utf-8", (error) => {
+  if (error) {
+    console.log("error: ", error.message);
+  } else {
+    console.log("Ghi file thành công");
+  }
 });
 
-server.listen(8080, "127.0.0.1", () => {
-  console.log("Máy chủ đang chạy. Vui lòng truy cấp http://127.0.0.1:8080/");
-});
+console.log("Phía dưới hàm writeFile()");
