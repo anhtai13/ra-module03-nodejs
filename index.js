971aa6152caa96c2dc5949f56ffc69b0893b9470
anhtai13 <tainguyen2322001@gmail.com>
2023-07-21 18:49:57 +0700

    session 03 | Bai tap 2 + 3

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const fs = require("fs");
 
-const content = "Tín\nThái\nTrung\nSáng\nViệt Plus\nSơn\nTài\nGiang";
-fs.writeFile("test.txt", content, "utf-8", (error) => {
-  if (error) {
-    console.log("error: ", error.message);
-  } else {
-    console.log("Ghi file thành công");
-  }
-});
+const students = [
+  {
+    name: "Thái",
+    age: 28,
+    hometown: " Đà Nẵng",
+  },
+  {
+    name: "Tín",
+    age: 29,
+    hometown: " Quảng Nam",
+  },
+  {
+    name: "Tài",
+    age: 22,
+    hometown: "Quảng Nam",
+  },
+  {
+    name: "Giang",
+    age: 22,
+    hometown: "Quảng Bình",
+  },
+  {
+    name: "Phương",
+    age: 27,
+    hometown: "",
+  },
+  { name: "Sáng", age: 21, hometown: "" },
+  { name: "Việt", age: 22, hometown: "" },
+];
 
-console.log("Phía dưới hàm writeFile()");
+// // Ghi nhiều files - tên mỗi file là tên mỗi học viên trong lớp, nội dung file chứa tên, tuổi, quê quán của học viên tương ứng
+// for (let student of students) {
+//   const content =
+//     "Tên: " +
+//     student.name +
+//     "\n" +
+//     "Tuổi: " +
+//     student.age +
+//     "\n" +
+//     "Quê quán: " +
+//     student.hometown;
+//   fs.writeFileSync(student.name + ".txt", content, "utf-8");
+// }
+
+// Đọc files và in ra console thông tin của từng học viên
+for (let student of students) {
+  const fileName = student.name + ".txt";
+  const fileContent = fs.readFileSync(fileName, "utf-8");
+  console.log(fileContent);
+}
