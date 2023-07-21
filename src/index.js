1ec1dd24b9d0e0ca5cc4b7e40cd98d7d966400d8
anhtai13 <tainguyen2322001@gmail.com>
2023-07-21 19:20:54 +0700

    session 04 | create express server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// const express = require("express");
+
+import express from "express"; //Phải thêm "type": "module" ở package.json,
+
+const application = express();
+
+application.listen(8000, () => {
+  console.log("server started");
+});
