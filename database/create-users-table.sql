a4cba1c6003cf47a73388e2cac3c3d8890807734
anhtai13 <tainguyen2322001@gmail.com>
2023-07-31 19:01:38 +0700

    session 08 | start/stop mysql basic

--- database/create-users-table.sql
+++ database/create-users-table.sql
@@ -12,3 +12,28 @@ CREATE TABLE `users`(
     `updated_at` DATETIME,
     `updated_by` INT
 );
+
+CREATE TABLE `products`(
+	`id` INT auto_increment primary key,
+    `name_product` VARCHAR(20) NOT NULL,
+    `description` VARCHAR(20) NOT NULL,
+    `quantity` VARCHAR(255) NOT NULL,
+    `unit_price` VARCHAR(50),
+    `avatar` VARCHAR(255)
+);
+
+CREATE TABLE `orders`(
+	`id` INT auto_increment primary key,
+    `name_product` VARCHAR(20) NOT NULL,
+    `description` VARCHAR(20) NOT NULL,
+    `quantity` VARCHAR(255) NOT NULL,
+    `status` VARCHAR(255) NOT NULL
+    `unit_price` NUMBER(50),
+    `total` NUMBER(50),
+    `avatar` VARCHAR(255),
+    `created_at` DATETIME,
+    `created_by` INT,
+    `updated_at` DATETIME,
+    `updated_by` INT
+);
+
---
+++ database/insert_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users`
+(`username`,`email`,`password`, `first_name`, `last_name`, `role`, `avatar`, `created_at`, `created_by`, `updated_at`, `updated_by`)
+VALUES ('tintt', 'tintt@gmail.com', 'pw123', 'Tin', 'Tran Trong', 1, 'avatar', NOW(), 1, NOW(),2);
